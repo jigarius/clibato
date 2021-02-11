e1b7b2621c514e87f2e3eeca84789e3b18490de6
Jigar Mehta <hello@jigarius.com>
2021-02-11 00:06:40 -0500

    Create self._config from .clibato.yml

--- clibato/cli.py
+++ clibato/cli.py
@@ -1,39 +1,42 @@
-"""Clibato CLI"""
-
 import argparse
+from clibato import Config
 
 
 class CLI:
     """Clibato CLI"""
-    _args = None
-
     def __init__(self):
-        pass
+        self._args = None
+        self._config = None
 
     def execute(self):
         """Executes the CLI"""
         self._args = CLI._argparser().parse_args()
-        self.log(f'Action: {self._args.action}')
 
+        self._log(f'Action: {self._args.action}')
         method = getattr(self, self._args.action)
         method()
 
     def init(self):
         """Action: Initialize configuration"""
-        self.log('TODO: init')
+        self._log('TODO: init')
 
     def backup(self):
         """Action: Create backup"""
-        self.log('TODO: backup')
+        self._log('TODO: backup')
 
     def restore(self):
         """Action: Restore backup"""
-        self.log('TODO: restore')
+        self._log('TODO: restore')
 
-    def log(self, message):
+    def _log(self, message):
         """Logs to STDOUT"""
         if self._args.verbose:
             print('[debug]', message)
+
+    def _ensure_config(self):
+        if not self._config:
+            self._log(f'Loading configuration: {self._args.config_file}')
+            self._config = Config.from_file(self._args.config_file)
 
     @staticmethod
     def _argparser():
@@ -46,6 +49,13 @@ class CLI:
             action='store_true',
             dest='verbose',
             help='Enable verbose output.'
+        )
+        common_parser.add_argument(
+            '--config-file',
+            default='~/.clibato.yml',
+            action='store',
+            dest='config_file',
+            help='A Clibato configuration file (YML).'
         )
 
         subparsers = main_parser.add_subparsers(dest='action')
