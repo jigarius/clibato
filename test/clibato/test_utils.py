3d77a6496770cdc9c67fe8b3087575c3724f1d2a
Jigar Mehta <hello@jigarius.com>
2021-02-13 23:04:40 -0500

    Remove ConfigDict and dict_merge()

--- test/clibato/test_utils.py
+++ test/clibato/test_utils.py
@@ -5,52 +5,6 @@ from clibato import utils
 
 
 class TestUtils(unittest.TestCase):
-    def test_dict_merge(self):
-        dict1 = {
-            'a': 'alpha',
-            'b': '',
-            'd': {
-                '1': 'one',
-                '2': '',
-            },
-            'e': 'error',
-        }
-
-        dict2 = {
-            'a': '',
-            'b': 'beta',
-            'c': 'charlie',
-            'd': {
-                '2': 'two',
-                '3': 'three'
-            },
-            'e': 'echo'
-        }
-
-        expectation = {
-            'a': 'alpha',  # Keeps dict1 value since dict2 value is empty.
-            'b': 'beta',
-            'c': 'charlie',
-            'd': {
-                '1': 'one',
-                '2': 'two',
-                '3': 'three'
-            },
-            'e': 'echo'
-        }
-
-        self.assertEqual(
-            utils.dict_merge(dict1, dict2),
-            expectation
-        )
-
-    def test_normalize_path(self):
-        """Path ~/foo/bar becomes $HOME/foo/bar"""
-        self.assertEqual(
-            utils.normalize_path('~/foo/bar'),
-            os.path.expanduser('~/foo/bar')
-        )
-
     @unittest.skip('Create TestEnsureShape')
     def test_ensure_shape(self):
         pass
