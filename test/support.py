f7b4860595bd585f9112d09d42dd8bd7d435bebf
Jigar Mehta <hello@jigarius.com>
2021-02-16 16:45:19 -0500

    Create file-system assertions

--- test/support.py
+++ test/support.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import logging
+import pathlib
 import unittest
 
 
@@ -11,7 +12,7 @@ class FileSystem:
         raise NotImplementedError()
 
     @staticmethod
-    def write_file(path: str, data: str = '', append=False) -> None:
+    def write_file(path: pathlib.Path, data: str = '', append=False):
         """
         Writes data to a file.
 
@@ -20,22 +21,26 @@ class FileSystem:
         :param append: Whether to write in append mode.
         :return: None
         """
-        path = os.path.expanduser(path)
+        path = FileSystem._normalize_path(path)
         mode = 'a' if append else 'w'
+
+        parent = FileSystem._normalize_path(os.path.dirname(path))
+        if not parent.is_dir():
+            parent.mkdir()
 
         file = open(path, mode)
         file.write(data)
         file.close()
 
     @staticmethod
-    def read_file(path: str) -> str:
+    def read_file(path: pathlib.Path) -> str:
         """
         Read data from a file.
 
         :param path: File path.
         :return: File contents.
         """
-        path = os.path.expanduser(path)
+        path = FileSystem._normalize_path(path)
 
         file = open(path, 'r')
         result = file.read()
@@ -44,35 +49,26 @@ class FileSystem:
         return result
 
     @staticmethod
-    def ensure(path: str) -> None:
-        """
-        Ensures that the directory exists. If it doesn't exist, it is created.
-
-        Paths starting with ~/ are automatically expanded.
-
-        :param path: Path to directory.
-        :return: None
-        """
-        path = os.path.expanduser(path)
-        if os.path.isdir(path):
-            return
-
-        os.mkdir(path)
-
-    @staticmethod
-    def remove(path: str) -> None:
+    def remove(path: pathlib.Path) -> None:
         """
         Removes the specified file or directory.
 
         :param path: Path to a file or a directory
         :return: None
         """
-        path = os.path.expanduser(path)
+        path = FileSystem._normalize_path(path)
 
-        if os.path.isdir(path):
+        if path.is_dir():
             shutil.rmtree(path)
-        elif os.path.isfile(path):
-            os.remove(path)
+        elif path.is_file():
+            path.unlink()
+
+    @staticmethod
+    def _normalize_path(path) -> pathlib.Path:
+        if not isinstance(path, pathlib.Path):
+            path = pathlib.Path(path)
+
+        return path.expanduser()
 
 
 class TestCase(unittest.TestCase):
@@ -101,3 +97,38 @@ class TestCase(unittest.TestCase):
         real = {'level': record.levelname, 'message': record.message}
 
         self.assertEqual(expectation, real, 'Log record mismatch')
+
+    def assert_file_exists(self, path: pathlib.Path) -> None:
+        """
+        Asserts that the file exists.
+
+        :param path: File path.
+        :return: None.
+        """
+        if not isinstance(path, pathlib.Path):
+            path = pathlib.Path(path)
+
+        self.assertTrue(path.is_file(), f"Expected file to exist, but it doesn't exist: {path}")
+
+    def assert_file_not_exists(self, path: pathlib.Path) -> None:
+        """
+        Asserts that the file doesn't exist.
+
+        :param path: File path.
+        :return: None.
+        """
+        if not isinstance(path, pathlib.Path):
+            path = pathlib.Path(path)
+
+        self.assertFalse(path.is_file(), f"Expected file to not exist, but it exists: {path}")
+
+    def assert_file_contents(self, path: pathlib.Path, contents) -> None:
+        """
+        Asserts whether the file has the expected contents.
+
+        :param path: File path.
+        :param contents: Expected contents.
+        :return: None.
+        """
+        self.assert_file_exists(path)
+        self.assertEqual(contents, FileSystem.read_file(path))
