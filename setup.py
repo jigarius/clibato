3178b563c976d713c686ed941f8dd5bdfbfde1b4
Jigar Mehta <hello@jigarius.com>
2021-02-15 20:18:02 -0500

    Prepare for PyPi release; refs #13

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    install_requires = fh.read().splitlines()
+
+github_url = "https://github.com/jigarius/clibato"
+author_url = "https://jigarius.com/"
+
+setuptools.setup(
+    name="clibato",
+    version="0.5.0.dev1",
+    author="Jigar Mehta",
+    author_email="hello@jigarius.com",
+    description="A tool to backup/restore simple files, e.g. dot files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='GNU LGPL v2.1',
+    url=github_url,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+    keywords='cli backup restore dotfile utility',
+    project_urls={
+        "Documentation": github_url + '#readme',
+        "Author": author_url,
+        "Tracker": "https://github.com/jigarius/clibato/issues"
+    },
+    packages=setuptools.find_packages(exclude=['test']),
+    python_requires='>=3.5',
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': ['clibato=clibato.__main__:main'],
+    }
+)
